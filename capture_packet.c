da3ae96336ca3be4fa77822c21849ebe9375f2a3
Skander Mejdi <mejdi.skander@gmail.com>
2017-11-17 22:50:33 +0100

    begin graphic part

--- capture_packet.c
+++ capture_packet.c
@@ -4,22 +4,25 @@
 #include<netinet/in.h>
 #include<errno.h>
 #include<netdb.h>
-#include<stdio.h> 
-#include<stdlib.h>   
-#include<string.h>  
- 
-#include<netinet/ip_icmp.h> 
-#include<netinet/udp.h>   
-#include<netinet/tcp.h>  
-#include<netinet/ip.h>   
-#include<netinet/if_ether.h>  
-#include<net/ethernet.h> 
+#include<stdio.h>
+#include<ncurses.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<netinet/ip_icmp.h>
+#include<netinet/udp.h>
+#include<netinet/tcp.h>
+#include<netinet/ip.h>
+#include<netinet/if_ether.h>
+#include<net/ethernet.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<sys/ioctl.h>
 #include<sys/time.h>
 #include<sys/types.h>
 #include<unistd.h>
+
+static int y = 0;
 
 int isAlphaNumberic(char c)
 {
@@ -29,182 +32,183 @@ int isAlphaNumberic(char c)
 }
 
 
-void PrintData (char* data , int Size)
+void PrintData (char* data , int Size, WINDOW *win)
 {
     int i , j;
     for(i=0 ; i < Size ; i++){
         if(i!=0 && i% LINE_LENGTH == 0){
-            printf("      ");
+            mvwprintw(win, y, 1, "      ");
             for(j=i-16 ; j<i ; j++)
-                (isAlphaNumberic(data[j])) ? printf("%c",(unsigned char)data[j]) : printf(".");
-            printf("\n");
+                (isAlphaNumberic(data[j])) ? mvwprintw(win, y, 1, "%c",(unsigned char)data[j]) : mvwprintw(win, y, 1, ".");
+            mvwprintw(win, y, 1, "\n");
         }
-        if(i% LINE_LENGTH == 0) 
-            printf("  ");
-        printf(" %02X",(unsigned int)data[i]);
+        if(i% LINE_LENGTH == 0)
+            mvwprintw(win, y, 1, "  ");
+        mvwprintw(win, y, 1, " %02X",(unsigned int)data[i]);
   	}
  }
 
-void print_ethernet_header(char* buffer, int size)
+void print_ethernet_header(char* buffer, int size, WINDOW *win)
 {
-    struct ethhdr *eth = (struct ethhdr *)buffer; 
-	printf("Ethernet Header\n");
-    printf("   |-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5] );
-    printf("   |-Source Address      : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5] );
-    printf("   |-Protocol            : %u\n",(unsigned short)eth->h_proto);
+    struct ethhdr *eth = (struct ethhdr *)buffer;
+	mvwprintw(win, y, 1, "Ethernet Header\n");
+    mvwprintw(win, y, 1, "   |-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5] );
+    mvwprintw(win, y, 1, "   |-Source Address      : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5] );
+    mvwprintw(win, y, 1, "   |-Protocol            : %u\n",(unsigned short)eth->h_proto);
 }
 
-void print_ip_header(char* buffer, int size)
+void print_ip_header(char* buffer, int size, WINDOW *win)
 {
 	struct sockaddr_in source, dest;
-    print_ethernet_header(buffer, size);
+    print_ethernet_header(buffer, size, win);
     short ipheader_len;
     struct iphdr *ip_header = (struct iphdr *)(buffer  + sizeof(struct ethhdr) );
     ipheader_len = ip_header->ihl * 4;
-    source.sin_addr.s_addr = ip_header->saddr;     
+    source.sin_addr.s_addr = ip_header->saddr;
     dest.sin_addr.s_addr = ip_header->daddr;
-   
-    printf("IP Header\n");
-    printf("   |-IP Version        : %d\n",(unsigned int)ip_header->version);
-    printf("   |-IP Header Length  : %d Bytes\n",((unsigned int)(ip_header->ihl))*4);
-    printf("   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(ip_header->tot_len));
-    printf("   |-Identification    : %d\n",ntohs(ip_header->id));
-    printf("   |-Protocol : %d\n",(unsigned int)ip_header->protocol);
-    printf("   |-TTL      : %d\n",(unsigned int)ip_header->ttl);
-	printf("   |-Type Of Service   : %d\n",(unsigned int)ip_header->tos);
-   	printf("   |-Checksum : %d\n",ntohs(ip_header->check));
-    printf("   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
-    printf("   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
+
+    mvwprintw(win, y, 1, "IP Header\n");
+    mvwprintw(win, y, 1, "   |-IP Version        : %d\n",(unsigned int)ip_header->version);
+    mvwprintw(win, y, 1, "   |-IP Header Length  : %d Bytes\n",((unsigned int)(ip_header->ihl))*4);
+    mvwprintw(win, y, 1, "   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(ip_header->tot_len));
+    mvwprintw(win, y, 1, "   |-Identification    : %d\n",ntohs(ip_header->id));
+    mvwprintw(win, y, 1, "   |-Protocol : %d\n",(unsigned int)ip_header->protocol);
+    mvwprintw(win, y, 1, "   |-TTL      : %d\n",(unsigned int)ip_header->ttl);
+	mvwprintw(win, y, 1, "   |-Type Of Service   : %d\n",(unsigned int)ip_header->tos);
+   	mvwprintw(win, y, 1, "   |-Checksum : %d\n",ntohs(ip_header->check));
+    mvwprintw(win, y, 1, "   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
+    mvwprintw(win, y, 1, "   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
 
 
 }
 
-void print_icmp_packet(char* buffer, int size)
+void print_icmp_packet(char* buffer, int size, WINDOW *win)
 {
 
     struct iphdr *ip_header = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-    unsigned short header_len = ip_header->ihl * 4; 
+    unsigned short header_len = ip_header->ihl * 4;
     struct icmphdr *icmp_header = (struct icmphdr *)(buffer + header_len + sizeof(struct ethhdr));
-    int header_size =  sizeof(struct ethhdr) + header_len + sizeof icmp_header; 
+    int header_size =  sizeof(struct ethhdr) + header_len + sizeof icmp_header;
 
-    printf("\nICMP Packet\n"); 
-     
-    print_ip_header(buffer, size);
-    printf("\nICMP Header\n");
-    printf("   |-Type : %d",(unsigned int)(icmp_header->type));
-             
+    mvwprintw(win, y, 1, "\nICMP Packet\n");
+
+    print_ip_header(buffer, size, win);
+    mvwprintw(win, y, 1, "\nICMP Header\n");
+    mvwprintw(win, y, 1, "   |-Type : %d",(unsigned int)(icmp_header->type));
+
     if((unsigned int)(icmp_header->type) == 11)
-        printf("  (TTL Expired)\n");
+        mvwprintw(win, y, 1, "  (TTL Expired)\n");
     else if((unsigned int)(icmp_header->type) == ICMP_ECHOREPLY)
-        printf("  (ICMP Echo Reply)\n");
+        mvwprintw(win, y, 1, "  (ICMP Echo Reply)\n");
 
-    printf("   |-Code : %d\n",(unsigned int)(icmp_header->code));
-    printf("   |-Checksum : %d\n",ntohs(icmp_header->checksum));
-    printf("\nIP Header\n");
-    PrintData(buffer, header_len);
-    printf("\nUDP Header\n");
-    PrintData(buffer + header_len , sizeof (icmp_header));
-    printf("\nData Payload\n");
-    PrintData(buffer + header_size , (size - header_size) );
-    printf("\n###########################################################");
+    mvwprintw(win, y, 1, "   |-Code : %d\n",(unsigned int)(icmp_header->code));
+    mvwprintw(win, y, 1, "   |-Checksum : %d\n",ntohs(icmp_header->checksum));
+    mvwprintw(win, y, 1, "\nIP Header\n");
+    PrintData(buffer, header_len, win);
+    mvwprintw(win, y, 1, "\nUDP Header\n");
+    PrintData(buffer + header_len , sizeof (icmp_header), win);
+    mvwprintw(win, y, 1, "\nData Payload\n");
+    PrintData(buffer + header_size , (size - header_size), win);
+    mvwprintw(win, y, 1, "\n###########################################################");
 }
 
-void print_tcp_packet(char *buffer, int size)
+void print_tcp_packet(char *buffer, int size, WINDOW *win)
 {
     struct iphdr *ip_header = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-    unsigned short header_len = ip_header->ihl * 4; 
+    unsigned short header_len = ip_header->ihl * 4;
     struct tcphdr *tcp_header=(struct tcphdr*)(buffer + header_len + sizeof(struct ethhdr));
-             
-    int header_size =  sizeof(struct ethhdr) + header_len + tcp_header->doff*4;
-     
-    printf("TCP Packet\n");
 
-    print_ip_header(buffer, size);
-         
-    printf("\n");
-    printf("TCP Header\n");
-    printf("   |-Source Port      : %u\n",ntohs(tcp_header->source));
-    printf("   |-Destination Port : %u\n",ntohs(tcp_header->dest));
-    printf("   |-Sequence Number    : %u\n",ntohl(tcp_header->seq));
-    printf("   |-Acknowledge Number : %u\n",ntohl(tcp_header->ack_seq));
-    printf("   |-Header Length      : %d BYTES\n", (unsigned int)tcp_header->doff*4);
-    printf("   |-Urgent Flag          : %d\n",(unsigned int)tcp_header->urg);
-    printf("   |-Acknowledgement Flag : %d\n",(unsigned int)tcp_header->ack);
-    printf("   |-Synchronise Flag     : %d\n",(unsigned int)tcp_header->syn);
-    printf("   |-Window         : %d\n",ntohs(tcp_header->window));
-    printf("   |-Checksum       : %d\n",ntohs(tcp_header->check));
-    printf("   |-Urgent Pointer : %d\n",tcp_header->urg_ptr);
-    printf("\nIP Header\n");
-    PrintData(buffer, header_len);         
-    printf("\nTCP Header\n");
-    PrintData(buffer + header_len,tcp_header->doff*4);
-    printf("\nData Payload\n");    
-    PrintData(buffer + header_size , size - header_size );
-    printf("\n###########################################################\n");
+    int header_size =  sizeof(struct ethhdr) + header_len + tcp_header->doff*4;
+
+    mvwprintw(win, y, 1, "TCP Packet\n");
+
+    print_ip_header(buffer, size, win);
+
+    mvwprintw(win, y, 1, "\n");
+    mvwprintw(win, y, 1, "TCP Header\n");
+    mvwprintw(win, y, 1, "   |-Source Port      : %u\n",ntohs(tcp_header->source));
+    mvwprintw(win, y, 1, "   |-Destination Port : %u\n",ntohs(tcp_header->dest));
+    mvwprintw(win, y, 1, "   |-Sequence Number    : %u\n",ntohl(tcp_header->seq));
+    mvwprintw(win, y, 1, "   |-Acknowledge Number : %u\n",ntohl(tcp_header->ack_seq));
+    mvwprintw(win, y, 1, "   |-Header Length      : %d BYTES\n", (unsigned int)tcp_header->doff*4);
+    mvwprintw(win, y, 1, "   |-Urgent Flag          : %d\n",(unsigned int)tcp_header->urg);
+    mvwprintw(win, y, 1, "   |-Acknowledgement Flag : %d\n",(unsigned int)tcp_header->ack);
+    mvwprintw(win, y, 1, "   |-Synchronise Flag     : %d\n",(unsigned int)tcp_header->syn);
+    mvwprintw(win, y, 1, "   |-Window         : %d\n",ntohs(tcp_header->window));
+    mvwprintw(win, y, 1, "   |-Checksum       : %d\n",ntohs(tcp_header->check));
+    mvwprintw(win, y, 1, "   |-Urgent Pointer : %d\n",tcp_header->urg_ptr);
+    mvwprintw(win, y, 1, "\nIP Header\n");
+    PrintData(buffer, header_len, win);
+    mvwprintw(win, y, 1, "\nTCP Header\n");
+    PrintData(buffer + header_len,tcp_header->doff*4, win);
+    mvwprintw(win, y, 1, "\nData Payload\n");
+    PrintData(buffer + header_size , size - header_size, win);
+    mvwprintw(win, y, 1, "\n###########################################################\n");
 }
 
-void print_udp_packet(char *buffer, int size)
+void print_udp_packet(char *buffer, int size, WINDOW *win)
 {
-     
+
     struct iphdr *ip_header = (struct iphdr *)(buffer + sizeof(struct ethhdr));
    	unsigned short header_len = ip_header->ihl * 4;
     struct udphdr *udp_header = (struct udphdr*)(buffer + header_len  + sizeof(struct ethhdr));
     int header_size =  sizeof(struct ethhdr) +header_len + sizeof udp_header;
-     
-    printf("\nUDP Packet\n");
-    print_ip_header(buffer, size);           
-     
-    printf("\nUDP Header\n");
-    printf("   |-Source Port      : %d\n" , ntohs(udp_header->source));
-    printf("   |-Destination Port : %d\n" , ntohs(udp_header->dest));
-    printf("   |-UDP Length       : %d\n" , ntohs(udp_header->len));
-    printf("   |-UDP Checksum     : %d\n" , ntohs(udp_header->check));
-    printf("\nIP Header\n");
-    PrintData(buffer , header_len);
-    printf("\nUDP Header\n");
-    PrintData(buffer + header_len , sizeof(udp_header));
-    printf("\nData Payload\n");    
-    PrintData(buffer + header_size , size - header_size);
-    printf("\n###########################################################");
+
+    mvwprintw(win, y, 1, "\nUDP Packet\n");
+    print_ip_header(buffer, size, win);
+
+    mvwprintw(win, y, 1, "\nUDP Header\n");
+    mvwprintw(win, y, 1, "   |-Source Port      : %d\n" , ntohs(udp_header->source));
+    mvwprintw(win, y, 1, "   |-Destination Port : %d\n" , ntohs(udp_header->dest));
+    mvwprintw(win, y, 1, "   |-UDP Length       : %d\n" , ntohs(udp_header->len));
+    mvwprintw(win, y, 1, "   |-UDP Checksum     : %d\n" , ntohs(udp_header->check));
+    mvwprintw(win, y, 1, "\nIP Header\n");
+    PrintData(buffer , header_len, win);
+    mvwprintw(win, y, 1, "\nUDP Header\n");
+    PrintData(buffer + header_len , sizeof(udp_header), win);
+    mvwprintw(win, y, 1, "\nData Payload\n");
+    PrintData(buffer + header_size , size - header_size, win);
+    mvwprintw(win, y, 1, "\n###########################################################");
 }
 
-void check_packet(char* buffer, int size)
+void check_packet(char* buffer, int size, WINDOW *win)
 {
+  y = 0;
     struct iphdr *ip_header = (struct iphdr*)(buffer + sizeof(struct ethhdr));
  	if (ip_header->protocol == 1)//icmp
- 		print_icmp_packet( buffer , size);
+ 		print_icmp_packet( buffer , size, win);
     else if (ip_header->protocol == 6)//tcp
-        print_tcp_packet(buffer , size);
+        print_tcp_packet(buffer , size, win);
     else if (ip_header->protocol == 17)// udp
-        print_udp_packet(buffer , size);         
+        print_udp_packet(buffer , size, win);
     else
-    	printf("Unknown type of packet");
+    	mvwprintw(win, y, 1, "Unknown type of packet");
 }
 
-void capture_packet(){
+// void capture_packet(){
+//
+// 	int data_size;
+// 	int raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+// 	struct sockaddr socket_addr;
+// 	int sockaddr_size = sizeof(socket_addr);
+//
+// 	 if(raw_socket < 0){
+//         perror("Socket Error");
+//         exit(1);
+//     }
+//    	char *buffer = malloc(sizeof(char *) * MAX_LENGTH_PACKET);
+// 	while(1){
+//         data_size = recvfrom(raw_socket, buffer, MAX_LENGTH_PACKET, 0, &socket_addr, (socklen_t*)&sockaddr_size);
+//         if(data_size <0 ){
+//             mvwprintw(win, y, 1, "Recvfrom error\n");
+//             exit(1);
+//         }
+//         check_packet(buffer , data_size);
+//     }
+//     close(raw_socket);
+// }
 
-	int data_size;
-	int raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-	struct sockaddr socket_addr;
-	int sockaddr_size = sizeof(socket_addr);
-
-	 if(raw_socket < 0){
-        perror("Socket Error");
-        exit(1);
-    }
-   	char *buffer = malloc(sizeof(char *) * MAX_LENGTH_PACKET);
-	while(1){
-        data_size = recvfrom(raw_socket, buffer, MAX_LENGTH_PACKET, 0, &socket_addr, (socklen_t*)&sockaddr_size);
-        if(data_size <0 ){
-            printf("Recvfrom error\n");
-            exit(1);
-        }
-        check_packet(buffer , data_size);
-    }
-    close(raw_socket);
-}
-
-int main()
-{
-	capture_packet();
-}
+// int main()
+// {
+// 	capture_packet();
+// }
